692435f517e50c245ebf534a7e07eba89525cafe
bowmans.dev <bowmans.dev@gmail.com>
2022-12-27 08:24:33 +0000

    🐣🐥

---
+++ index.js
@@ -0,0 +1,32 @@
+const http = require('http');
+const WebSocketServer = require('websocket').server;
+let connection = null;
+
+const httpserver = http.createServer((req, res) => {
+    console.log("We have recieved a request");
+});
+
+const websocket = new WebSocketServer({ "httpServer": httpserver }); 
+
+httpserver.listen(8080, () => {
+    console.log("Server is listening on port 8080");
+});
+
+websocket.on("request", request=> {
+    connection = request.accept(null, request.origin);
+    console.log("Connection accepted");
+    
+    connection.on("open", () => console.log("Connection opened"));
+    connection.on("close", () => console.log("Connection closed"));
+    connection.on('message', (message) => {
+        console.log("Message recieved");
+        console.log(`Recieved message: ${message.utf8Data}`);
+    });
+    sendevery5seconds();
+
+});
+      
+function sendevery5seconds() {
+    connection.sendUTF(`Message ${Math.random()}`);
+    setTimeout(sendevery5seconds, 5000);
+}
